cac5042d59b50df83954c859599476b566ab46a1
Guilherme Picasso <guipicasso3@gmail.com>
2024-04-24 22:19:48 -0300

    painel Controle 2/2 + correções

--- front/src/Pages/Home/index.js
+++ front/src/Pages/Home/index.js
@@ -3,13 +3,17 @@ import Cardapio from '../Cardapio';
 
 import { Link } from 'react-router-dom';
 
-function Home() {
+export default function Home() {
   return (
     <div className="pagina-home">
 
       <Link to="./cardapio">Cardápio</Link>
+      <Link to="./painel_controle">Painel_Controle</Link>
+      <Link to="./painel_controle/cardapios">Painel_Controle_cardapios</Link>
+      
+      
     </div>
   );
 }
 
-export default Home;
+
